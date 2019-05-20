fe60a1d08539244221584b6d02836ce8fe3e5c35
doleron <doleron@gmail.com>
2019-05-20 11:01:46 -0300

    update to vectorized version

--- examples/chapter_three/gradient_descent_example.cpp
+++ examples/chapter_three/gradient_descent_example.cpp
@@ -17,7 +17,7 @@
 
 std::tuple<double, double, double, double> gradientDescent(Matrix &X, Matrix &T, double learningRate, int maxEpochs, double minCost);
 std::random_device rd;
-std::mt19937 prn(rd());
+std::mt19937 prn(0);
 std::uniform_real_distribution<> uniformDist(-1.0, 1.0);
 
 ann::LogisticActivationFunction g;
@@ -26,13 +26,10 @@ std::tuple<Matrix, Matrix> makeSyntheticDataset()
 {
     Matrix T(1, 20);
     T << 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0;
-
     std::uniform_real_distribution<> randUniform(0.0, 1.0);
-
     Matrix X = T.unaryExpr([&randUniform](double t){
         return 2.0 + 3 * t + randUniform(prn);
     });
-
     return std::make_tuple(X, T);
 }
 
@@ -50,21 +47,22 @@ double quadraticCost(const Matrix & X, const Matrix & T, double w, double b)
 int main(int, char **)
 {
     int epochs = 500;
-    double learningRate = 5;
+    double learningRate = 15;
     double costThreshold = 1e-2;
     auto [X, T] = makeSyntheticDataset();
-    std::cout << "The max number of epochs is " << epochs << "\n";
-    std::cout << "Learning rate is " << learningRate << "\n";
-    std::cout << "The target cost is " << costThreshold << "\n\n";
+    std::cout << "Max number of epochs: " << epochs << " | ";
+    std::cout << "Learning rate: " << learningRate << " | ";
+    std::cout << "Target cost: " << costThreshold << "\n";
+
     auto [epoch, cost, w, b] = gradientDescent(X, T, learningRate, epochs, costThreshold);
-    std::cout << "Reached epoch: " << epoch << "\n";
+    std::cout << "Reached epoch: " << epoch << " | ";
     std::cout << "Final cost: " << cost << "\n";
-    std::cout << "Final Weights: " << w << "\n";
+    std::cout << "Final Weights: " << w << " | ";
     std::cout << "Final bias: " << b << "\n";
     return 0;
 }
 
-double dCdz(double w, double b, double x, double t)
+double calc_dZ(double w, double b, double x, double t)
 {
     double z = x * w + b;
     double y = g(z);
@@ -72,22 +70,24 @@ double dCdz(double w, double b, double x, double t)
     return result;
 }
 
-std::tuple<double, double, double, double>  gradientDescent(Matrix &X, Matrix &T, const double learningRate, const int maxEpochs, const double minCost) {
-    double b = 0.0, w = 0.5 * uniformDist(prn);
+std::tuple<double, double, double, double>
+gradientDescent(Matrix &X, Matrix &T, double learningRate, int maxEpochs, double minCost) {
+    double b = 0.0;
+    double w = 0.5 * uniformDist(prn);
     double cost = quadraticCost(X, T, w, b);
-    int epoch = 0, m = X.cols();
-    while (epoch++ < maxEpochs && cost > minCost) {
-        double dW = 0.0, dB = 0.0;
-        for (int i = 0; i < m; ++i) {
-            double x = X(0, i), t = T(0, i);
-            double dz = dCdz(w, b, x, t);
-            dW += dz * x;
-            dB += dz;
-        }
-        dW /= m;     dB /= m;
-        w = w - learningRate * dW;
+    int epoch = 0;
+    while (epoch < maxEpochs && cost > minCost) {
+
+        auto dZ = X.binaryExpr(T, [&w, &b](double x, double t){
+            return calc_dZ(w, b, x, t);
+        });
+        double dW = dZ.cwiseProduct(X).mean();
+        double dB = dZ.mean();
+
+        w = w - learningRate * dW;   
         b = b - learningRate * dB;
         cost = quadraticCost(X, T, w, b);
+        epoch++;
     }
-    return std::make_tuple(--epoch, cost, w, b);
+    return std::make_tuple(epoch, cost, w, b);
 }
